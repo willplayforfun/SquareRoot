14f7117dae1746d5595a651b92d4e85526e23fd7
Matt Levonian <elevonian@gmail.com>
2015-12-12 00:00:12 -0800

    join screen working.

--- SquareRoot/Assets/Scripts/Menus/JoinScreenController.cs
+++ SquareRoot/Assets/Scripts/Menus/JoinScreenController.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using InControl;
+
+public class PlayerDeviceCoupling
+{
+    public PlayerNum number;
+    public InputDevice device;
+
+    public PlayerDeviceCoupling(PlayerNum p_num, InputDevice p_device)
+    {
+        number = p_num;
+        device = p_device;
+    }
+}
 
 public class JoinScreenController : MonoBehaviour
 {
+    private List<PlayerDeviceCoupling> players;
+
     public JoinPanel player1Pane;
     public JoinPanel player2Pane;
     public JoinPanel player3Pane;
@@ -18,75 +32,128 @@ public class JoinScreenController : MonoBehaviour
         player3Pane.Disappear();
         player4Pane.Disappear();
 
-        //InputManager.OnDeviceAttached += AddDevice;
-        //InputManager.OnDeviceDetached += AddDevice;
+        players = new List<PlayerDeviceCoupling>();
+
+        DontDestroyOnLoad(gameObject);
     }
 
     void Update()
     {
-        //if()
+        InputDevice inputDevice = InputManager.ActiveDevice;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (JoinButtonWasPressedOnDevice(inputDevice))
         {
-            AddDevice();
+            if (ThereIsNoPlayerUsingDevice(inputDevice))
+            {
+                AddDevice(inputDevice);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (LeaveButtonWasPressedOnDevice(inputDevice))
         {
-            RemoveDevice();
+            PlayerDeviceCoupling player = FindPlayerUsingDevice(inputDevice);
+            if (player != null)
+            {
+                RemovePlayer(player);
+            }
         }
     }
 
-    void AddDevice()
+    void OnDeviceDetached(InputDevice inputDevice)
     {
-        currentPlayerCount++;
-
-        switch (currentPlayerCount)
+        PlayerDeviceCoupling player = FindPlayerUsingDevice(inputDevice);
+        if (player != null)
         {
-            case 1:
-                player1Pane.Join();
-                player2Pane.Appear();
-                break;
-            case 2:
-                player2Pane.Join();
-                player3Pane.Appear();
-                break;
-            case 3:
-                player3Pane.Join();
-                player4Pane.Appear();
-                break;
-            case 4:
-                player4Pane.Join();
-                break;
-            default:
-                Debug.LogWarningFormat("Weird stuff inside JoinScreenController AddDevice() ({0} devices)", currentPlayerCount);
-                break;
+            RemovePlayer(player);
+        }
+    }
+
+    void RemovePlayer(PlayerDeviceCoupling player)
+    {
+        players.Remove(player);
+        RemoveDevice();
+    }
+
+    void AddDevice(InputDevice newDevice)
+    {
+        if (currentPlayerCount < 4)
+        {
+            players.Add(new PlayerDeviceCoupling((PlayerNum)currentPlayerCount, newDevice));
+
+            currentPlayerCount++;
+
+            switch (currentPlayerCount)
+            {
+                case 1:
+                    player1Pane.Join();
+                    player2Pane.Appear();
+                    break;
+                case 2:
+                    player2Pane.Join();
+                    player3Pane.Appear();
+                    break;
+                case 3:
+                    player3Pane.Join();
+                    player4Pane.Appear();
+                    break;
+                case 4:
+                    player4Pane.Join();
+                    break;
+            }
         }
     }
 
     void RemoveDevice()
     {
-        currentPlayerCount--;
-
-        switch (currentPlayerCount)
+        if (currentPlayerCount > 0)
         {
-            case 0:
-                player1Pane.Unjoin();
-                player2Pane.Disappear();
-                break;
-            case 1:
-                player2Pane.Unjoin();
-                player3Pane.Disappear();
-                break;
-            case 2:
-                player3Pane.Unjoin();
-                player4Pane.Disappear();
-                break;
-            case 3:
-                player4Pane.Unjoin();
-                break;
-            default:
-                Debug.LogWarningFormat("Weird stuff inside JoinScreenController RemoveDevice() ({0} devices)", currentPlayerCount);
-                break;
+            currentPlayerCount--;
+
+            switch (currentPlayerCount)
+            {
+                case 0:
+                    player1Pane.Unjoin();
+                    player2Pane.Disappear();
+                    break;
+                case 1:
+                    player2Pane.Unjoin();
+                    player3Pane.Disappear();
+                    break;
+                case 2:
+                    player3Pane.Unjoin();
+                    player4Pane.Disappear();
+                    break;
+                case 3:
+                    player4Pane.Unjoin();
+                    break;
+            }
         }
+    }
+
+    bool JoinButtonWasPressedOnDevice(InputDevice inputDevice)
+    {
+        return inputDevice.Action1.WasPressed || inputDevice.Command.WasPressed;
+    }
+
+    bool LeaveButtonWasPressedOnDevice(InputDevice inputDevice)
+    {
+        return inputDevice.Action2.WasPressed;
+    }
+
+    PlayerDeviceCoupling FindPlayerUsingDevice(InputDevice inputDevice)
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i].device == inputDevice)
+            {
+                return players[i];
+            }
+        }
+
+        return null;
+    }
+
+    bool ThereIsNoPlayerUsingDevice(InputDevice inputDevice)
+    {
+        return FindPlayerUsingDevice(inputDevice) == null;
     }
 }
