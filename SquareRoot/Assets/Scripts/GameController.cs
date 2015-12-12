b76738bfc11ace258c3efb5e2e459ac93c1a63ce
Matt Levonian <elevonian@gmail.com>
2015-12-11 21:36:14 -0800

    Player spawning and runtime split screen

---
+++ SquareRoot/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+    [Range(2,4)]
+    public int numPlayers;
+    public PlayerObject playerPrefab;
+
+    List<PlayerObject> players;
+
+    void Start()
+    {
+        players = new List<PlayerObject>();
+        for(int i = 0; i < numPlayers; i++)
+        {
+            PlayerObject player = Instantiate(playerPrefab);
+            player.SetPlayerNumber((PlayerNum)i, numPlayers);
+            players.Add(player);
+        }
+    }
+}
--- SquareRoot/Assets/Scripts/PlayerObject.cs
+++ SquareRoot/Assets/Scripts/PlayerObject.cs
@@ -1,7 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+public enum PlayerNum
+{
+    First,
+    Second,
+    Third,
+    Fourth
+}
+
 public class PlayerObject : MonoBehaviour
 {
     List<TendrilRoot> roots;
+
+    public void SetPlayerNumber(PlayerNum num, int numPlayers)
+    {
+        Camera camera = GetComponentInChildren<Camera>();
+        switch(num)
+        {
+            case PlayerNum.First:
+                if(numPlayers < 4)
+                {
+                    camera.rect = new Rect(0, 0.5f, 1, 0.5f);
+                }
+                else
+                {
+                    camera.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+                }
+                break;
+            case PlayerNum.Second:
+                if (numPlayers == 2)
+                {
+                    camera.rect = new Rect(0, 0, 1, 0.5f);
+                }
+                else if (numPlayers == 3)
+                {
+                    camera.rect = new Rect(0, 0, 0.5f, 0.5f);
+                }
+                else if (numPlayers == 4)
+                {
+                    camera.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+                }
+                break;
+            case PlayerNum.Third:
+                if (numPlayers == 4)
+                {
+                    camera.rect = new Rect(0, 0, 0.5f, 0.5f);
+                }
+                else
+                {
+                    camera.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+                }
+                break;
+            case PlayerNum.Fourth:
+                camera.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+                break;
+        }
+    }
 }
