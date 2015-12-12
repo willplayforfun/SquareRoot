e57e0ffcae2e54bfa5d459a0fda72dfbf30cd52f
Yohanes Shimelis <shimelis@usc.edu>
2015-12-11 20:52:19 -0800

    Created State script

---
+++ SquareRoot/Assets/Scripts/State.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class State : MonoBehaviour {
+	// Use this for initialization
+	virtual void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
