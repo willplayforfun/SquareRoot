814ed1c0f564ef8508917a88f9d1c5275f6676ed
Yohanes Shimelis <shimelis@usc.edu>
2015-12-14 18:17:55 -0800

    Fixes

--- SquareRoot/Assets/Scripts/Tendril/MeshMaker.cs
+++ SquareRoot/Assets/Scripts/Tendril/MeshMaker.cs
@@ -80,10 +80,10 @@ public class MeshMaker : MonoBehaviour
             tris.Add(end - 1);
         }
 
-        tendrilMesh.SetVertices(vertices);
-        tendrilMesh.uv = uvs.ToArray();
-        tendrilMesh.normals = normals.ToArray();
-        tendrilMesh.triangles = tris.ToArray();
+        //tendrilMesh.SetVertices(vertices);
+        //tendrilMesh.uv = uvs.ToArray();
+        //tendrilMesh.normals = normals.ToArray();
+        //tendrilMesh.triangles = tris.ToArray();
 
         tendrilMesh.RecalculateNormals();
         tendrilMesh.RecalculateBounds();
@@ -109,7 +109,7 @@ public class MeshMaker : MonoBehaviour
                 vertices[i] = Vector3.Lerp(vertices[i], average, progress);
                 vertices[i + 1] = Vector3.Lerp(vertices[i + 1], average, progress);
 
-                tendrilMesh.SetVertices(vertices);
+                //tendrilMesh.SetVertices(vertices);
             }
 
             yield return null;
